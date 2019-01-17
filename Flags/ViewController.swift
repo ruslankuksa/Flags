9a8d2d7a11524d937ba523269c00631218dbbadb
Руслан Кукса <serene.oldman@protonmail.ch>
2019-01-17 13:55:33 +0200

    Initial Commit

---
+++ Flags/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flags
+//
+//  Created by Руслан Кукса on 1/17/19.
+//  Copyright © 2019 Ruslan Kuksa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
